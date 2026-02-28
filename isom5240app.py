d30f7d6805ca61dff9a99f007392a4bb80079607
XX Gary <115163227+MrTher-77@users.noreply.github.com>
2026-02-28 17:08:12 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -3,11 +3,12 @@ from PIL import Image
 import streamlit as st
 
 # Streamlit UI
-st.title("Title: Age Classification using ViT")
+print("Title: Age Classification using ViT")
 
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification", model="nateraw/vit-age-classifier")
+age_classifier = pipeline("image-classification",
+                          model="nateraw/vit-age-classifier")
 
 image_name = "middleagedMan.jpg"
 image_name = Image.open(image_name).convert("RGB")
